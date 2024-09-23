85687e93b6bcf563cf11cbdf9981945eb33b8723
GusevADresume <resumeGusevAD@gmail.com>
2024-09-23 21:38:17 +0300

    add_model_new_data_and_migate

--- a_tOil/Oil/other_info/models.py
+++ a_tOil/Oil/other_info/models.py
@@ -4,13 +4,18 @@ from django.contrib.auth.models import User
 
 class UserInfo(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    table_access= models.BooleanField()
+    table_access = models.BooleanField()
     user_photo = models.ImageField()
     some_info = models.CharField(max_length=2500)
 
 
 class News(models.Model):
-
     news_header = models.CharField(max_length=50)
     news_body = models.CharField(max_length=500)
     news_image = models.ImageField()
+
+
+class NewData(models.Model):
+    header = models.CharField(max_length=50)
+    some_data = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True)
